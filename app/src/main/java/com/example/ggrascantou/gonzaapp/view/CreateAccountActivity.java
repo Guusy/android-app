50f8098906270a3bcb65aa82a635c95f86e6758f
Guusy <guusygonzalo@gmail.com>
2018-10-07 18:19:41 -0300

    start layout of login page

---
+++ app/src/main/java/com/example/ggrascantou/gonzaapp/view/CreateAccountActivity.java
@@ -0,0 +1,20 @@
+package com.example.ggrascantou.gonzaapp.view;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.ggrascantou.gonzaapp.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+
+}
