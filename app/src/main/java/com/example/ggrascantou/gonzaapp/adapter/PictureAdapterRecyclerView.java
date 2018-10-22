91306fb7a01680f8204f84a9b1beb70454fb4343
Guusy <guusygonzalo@gmail.com>
2018-10-21 22:12:19 -0300

    add pictures with picasso library

--- app/src/main/java/com/example/ggrascantou/gonzaapp/adapter/PictureAdapterRecyclerView.java
+++ app/src/main/java/com/example/ggrascantou/gonzaapp/adapter/PictureAdapterRecyclerView.java
@@ -1,4 +1,68 @@
 package com.example.ggrascantou.gonzaapp.adapter;
 
-public class PictureAdapterRecyclerView {
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.ggrascantou.gonzaapp.R;
+import com.example.ggrascantou.gonzaapp.model.Picture;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class PictureAdapterRecyclerView extends RecyclerView.Adapter<PictureAdapterRecyclerView.PictureViewHolder> {
+
+    public PictureAdapterRecyclerView(ArrayList<Picture> pictures, int resource, Activity activity) {
+        this.pictures = pictures;
+        this.resource = resource;
+        this.activity = activity;
+    }
+
+    private ArrayList<Picture> pictures;
+    private int resource;
+    private Activity activity;
+
+    @NonNull
+    @Override
+    public PictureViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(resource,parent,false);
+        return new PictureViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PictureViewHolder holder, int position) {
+        Picture picture = pictures.get(position);
+        holder.userNameCard.setText(picture.getUserName());
+        holder.timeCard.setText(picture.getTime());
+        holder.likeNumberCard.setText(picture.getLike_number());
+        Picasso.get().load(picture.getPicture()).into(holder.pictureCard);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return pictures.size();
+    }
+
+    public class PictureViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView pictureCard;
+        private TextView userNameCard;
+        private TextView timeCard;
+        private TextView likeNumberCard;
+
+
+        public PictureViewHolder(@NonNull View itemView) {
+            super(itemView);
+            pictureCard = (ImageView) itemView.findViewById(R.id.pictureCard);
+            userNameCard = (TextView) itemView.findViewById(R.id.userNameCard);
+            timeCard = (TextView) itemView.findViewById(R.id.timeCard);
+            likeNumberCard = (TextView) itemView.findViewById(R.id.likeNumberCard);
+        }
+    }
 }
--- app/src/main/java/com/example/ggrascantou/gonzaapp/view/fragments/HomeFragment.java
+++ app/src/main/java/com/example/ggrascantou/gonzaapp/view/fragments/HomeFragment.java
@@ -3,11 +3,20 @@ package com.example.ggrascantou.gonzaapp.view.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.ggrascantou.gonzaapp.R;
+import com.example.ggrascantou.gonzaapp.adapter.PictureAdapterRecyclerView;
+import com.example.ggrascantou.gonzaapp.model.Picture;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +33,31 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        showToolbar(getResources().getString(R.string.tab_home), false, view);
+        RecyclerView picturesRecycler = (RecyclerView) view.findViewById(R.id.pictureRecycler);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        picturesRecycler.setLayoutManager(linearLayoutManager);
+        PictureAdapterRecyclerView pictureAdapterRecyclerView = new PictureAdapterRecyclerView(this.buildPictures(), R.layout.cardview_picture, getActivity());
+        picturesRecycler.setAdapter(pictureAdapterRecyclerView);
+        return view;
     }
 
+    public ArrayList<Picture> buildPictures() {
+        ArrayList<Picture> pictures = new ArrayList<>();
+        pictures.add(new Picture("https://lamenteesmaravillosa.com/wp-content/uploads/2017/12/los-simpson-en-el-sofa.jpg", "Gonzalo gras cantou", "4 dias", "10 me gusta"));
+        pictures.add(new Picture("https://lamenteesmaravillosa.com/wp-content/uploads/2017/12/los-simpson-en-el-sofa.jpg", "Nicolas medela", "5 dias", "0 me gusta"));
+        pictures.add(new Picture("https://lamenteesmaravillosa.com/wp-content/uploads/2017/12/los-simpson-en-el-sofa.jpg", "Nicolas espindola", "20 dias", "120 me gusta"));
+        return pictures;
+    }
+
+    public void showToolbar(String title, boolean upButton, View view) {
+
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(title);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+    }
 }
