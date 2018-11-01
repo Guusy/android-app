cd708cbe5764cd807152bd25ebc071aa76eb7882
Guusy <guusygonzalo@gmail.com>
2018-10-31 22:19:00 -0300

    finish the transition between picture card and pictureDetail with back button

--- app/src/main/java/com/example/ggrascantou/gonzaapp/adapter/PictureAdapterRecyclerView.java
+++ app/src/main/java/com/example/ggrascantou/gonzaapp/adapter/PictureAdapterRecyclerView.java
@@ -1,6 +1,7 @@
 package com.example.ggrascantou.gonzaapp.adapter;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -11,21 +12,22 @@ import android.widget.TextView;
 
 import com.example.ggrascantou.gonzaapp.R;
 import com.example.ggrascantou.gonzaapp.model.Picture;
+import com.example.ggrascantou.gonzaapp.view.PictureDetailActivity;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 public class PictureAdapterRecyclerView extends RecyclerView.Adapter<PictureAdapterRecyclerView.PictureViewHolder> {
-
+    private ArrayList<Picture> pictures;
+    private int resource;
+    private Activity activity;
     public PictureAdapterRecyclerView(ArrayList<Picture> pictures, int resource, Activity activity) {
         this.pictures = pictures;
         this.resource = resource;
         this.activity = activity;
     }
 
-    private ArrayList<Picture> pictures;
-    private int resource;
-    private Activity activity;
+
 
     @NonNull
     @Override
@@ -41,6 +43,14 @@ public class PictureAdapterRecyclerView extends RecyclerView.Adapter<PictureAdap
         holder.timeCard.setText(picture.getTime());
         holder.likeNumberCard.setText(picture.getLike_number());
         Picasso.get().load(picture.getPicture()).into(holder.pictureCard);
+
+        holder.pictureCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(activity, PictureDetailActivity.class);
+                activity.startActivity(intent);
+            }
+        });
 
     }
 
